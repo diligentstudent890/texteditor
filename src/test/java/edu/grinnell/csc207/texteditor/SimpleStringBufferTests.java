b5b4d43eaabc0abdeef4eac7b482e54bfc628797
Brian He <brianhe@Brians-MacBook-Air.local>
2025-02-23 23:50:40 -0600

    Final fix I promise

--- src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
@@ -4,6 +4,7 @@ import net.jqwik.api.Property;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+import net.jqwik.api.ForAll;
 
 
 /**
